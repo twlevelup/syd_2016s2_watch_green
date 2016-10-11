f0793ceb32d620dbbddd38c5e78448cd28da7620
csghadinata@gmail.com <csghadinata@gmail.com>
2016-10-12 08:52:06 +1100

    [Clinton] Removed no longer necessary inputMode boolean and password array from checkInPage.js

--- client/src/js/pages/checkInPage.js
+++ client/src/js/pages/checkInPage.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var Page = require('watch_framework').Page;
-var inputMode = false;
 var passwordScreenMessage = 'Press the screen to Enter your password mode';
 
 var checkInPage = Page.extend({
@@ -13,37 +12,19 @@ var checkInPage = Page.extend({
     face: 'navigateToPasswordEntry'
   },
 
-  initialize: function() {
-    // this.inputPassword = [];
-    this.inputMode = false;
-  },
-
-  // appendUpToArray: function() {
-  //   this.inputPassword.push('U');
-  // },
-
   navigateToPasswordEntry: function() {
       window.App.navigate('passwordEntry');
     },
 
   render: function() {
-    if (inputMode === true) {
-      this.$el.html(this.template({
-        passwordPromptText: 'Enter the password'
-      }));
-    } else {
-      this.$el.html(this.template({
-        passwordPromptText: 'Press the screen to Enter your password mode'
-      }));
-    }
+
+    this.$el.html(this.template({
+      passwordPromptText: 'Press the screen to Enter your password mode'
+    }));
 
     return this;
 
   },
-
-  checkInputMode: function() {
-    return inputMode;
-  }
 
 });
 module.exports = checkInPage;
